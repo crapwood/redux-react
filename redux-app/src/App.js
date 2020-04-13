f83284b16d82d55e2ba7ff29dff36540969030b0
james <jjbe.93@gmail.com>
2020-04-13 23:48:18 +0300

    added gamification

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -10,9 +10,11 @@ function App() {
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-
+      {counter < 100 && <h3>Keep Adding till you get to 100 for a surprise</h3>}
       <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+
+      {counter > 100 && <h1>YOU WON!!!!</h1>}
     </div>
   );
 }
